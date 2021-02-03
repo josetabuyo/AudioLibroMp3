bc3b21eb377e87116e4ce61ed6be21a87b8039a5
José Tabuyo <jtabuyo@fusap.com.ar>
2021-02-03 19:23:39 -0300

    Primer commit Reproduce sonido con un tiempo de comienzo Puede poner pausa Falta mostrar y recordar el tiempo como next step

--- App.js
+++ App.js
@@ -1,15 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Audio } from 'expo-av';
+
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+var timeElapsed = Date.now();
+
+
+setInterval(function(){
+
+
+  timeElapsed = timeElapsed - Date.now();
+
+}, 1000);
 
 export default function App() {
+  const [sound, setSound] = React.useState();
+
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+      require('./assets/zelda-navi-listen.mp3')
+
+      // require('./assets/de-animales-a-dioses-yuval-noah-harari-audiolibro-parte-1.mp3')
+
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+
+    //
+    // <Text>{this.state.timeElapsed}</Text>
+    //
+    // this.setState({
+    //   timeElapsed: 'okokok'
+    // });
+
+    await sound.playAsync();
+    await sleep(200);
+
+    await sound.playFromPositionAsync(700);
+
+
+  }
+
+  async function stopSound() {
+    console.log('Stoping Sound');
+    await sound.pauseAsync();
+  }
+
+
+
+
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hola compañera Noelía, que gusto es verla este día</Text>
+      <Text>{timeElapsed}</Text>
+      <Button title="Play Sound" onPress={playSound} />
+      <Button title="Stop Sound" onPress={stopSound} />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
